7e2c6aa7b7c555cb9f49a6206693401b536cce91
Abigail Rosenfeld <abigail@MacBook-Air-de-Abigail.local>
2022-12-05 08:46:38 -0600

    comienzo del proyecto

---
+++ app.js
@@ -0,0 +1,16 @@
+function areadelcirculo(radio){
+    var pi=3.141592653589
+    return "el área del círulo es:" + pi*(radio**2)
+}
+console.log(areadelcirculo(9))
+
+function areadeltriangulo(base, altura){
+    return "el área del triángulo es:" + (base*altura)/2
+}
+console.log(areadeltriangulo(8,10))
+
+function areadelcuadrado(lado){
+    return "el área del cuadrado es" + (lado**2)
+}
+console.log(areadelcuadrado(20))
+
